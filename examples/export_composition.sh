3ad387776981a18f201d64225e4be439c1ae18d6
cj <cj>
2019-12-15 15:28:12 +0000

    initial

--- examples/export_composition.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Build
-cd ../
-cargo build --release
-
-# Run
-clear
-./target/release/chord_composer export ./examples/example_composition.yaml
--- examples/play_composition_zhn.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Build
-cd ../
-cargo build --release --features zhn
-
-# Run
-clear
-./target/release/chord_composer play ./examples/example_composition.yaml
